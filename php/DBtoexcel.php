739dc91fc875602090233934ef5bdccf5d63fb13
Meet57 <meet57.mp@gmail.com>
2021-05-03 16:03:51 +0530

    excel to csv

--- php/DBtoexcel.php
+++ php/DBtoexcel.php
@@ -1,42 +1,34 @@
 <?php
-    include_once './conn.php';    
-    
-    $DB_TBLName = "appointment"; 
-    $filename = "Appointment";  //your_file_name
-    $file_ending = "xls";   //file_extention
-    
-    header("Content-Type: application/xls");    
-    header("Content-Disposition: attachment; filename=$filename.$file_ending");  
-    header("Pragma: no-cache"); 
-    header("Expires: 0");
-    
-    $sep = "\t";
-    
-    $sql="SELECT * FROM $DB_TBLName order by id desc"; 
-    
-    $resultt = $conn->query($sql);
-    while ($property = mysqli_fetch_field($resultt)) { //fetch table field name
-        echo $property->name."\t";
+/*
+* iTech Empires:  Export Data from MySQL to CSV Script
+* Version: 1.0.0
+* Page: Export
+*/
+
+// Database Connection
+include_once "./conn.php";
+
+// get Users
+$query = "SELECT * FROM appointment order by id desc";
+if (!$result = mysqli_query($conn, $query)) {
+    exit(mysqli_error($conn));
+}
+
+$users = array();
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $users[] = $row;
     }
-    
-    print("\n");    
-    
-    while($row = mysqli_fetch_row($resultt))  //fetch_table_data
-    {
-        $schema_insert = "";
-        for($j=0; $j< mysqli_num_fields($resultt);$j++)
-        {
-            if(!isset($row[$j]))
-                $schema_insert .= "NULL".$sep;
-            elseif ($row[$j] != "")
-                $schema_insert .= "$row[$j]".$sep;
-            else
-                $schema_insert .= "".$sep;
-        }
-        $schema_insert = str_replace($sep."$", "", $schema_insert);
-        $schema_insert = preg_replace("/\r\n|\n\r|\n|\r/", " ", $schema_insert);
-        $schema_insert .= "\t";
-        print(trim($schema_insert));
-        print "\n";
+}
+
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=appointment.csv');
+$output = fopen('php://output', 'w');
+fputcsv($output, array('Id', 'Date', 'Time', 'Name', 'Status', 'Case', 'Village'));
+
+if (count($users) > 0) {
+    foreach ($users as $row) {
+        fputcsv($output, $row);
     }
+}
 ?>
